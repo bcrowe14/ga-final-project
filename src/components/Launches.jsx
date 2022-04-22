46adafd1b3c2dc220d138cf43b98e8677e85db5b
bcrowe14 <brooklyncrowe@rocketmail.com>
2022-04-21 23:05:57 -0400

    project started

---
+++ src/components/Launches.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Launches = (props) => {
+
+  
+
+    return (
+      <h2>Launch this </h2>
+    )
+  }
+
+  
+  export default Launches
